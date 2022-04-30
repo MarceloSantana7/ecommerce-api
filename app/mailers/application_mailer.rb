0a2edc451f39530728e9582e3b8d0d93231d7feb
Marcelo Santana <marcelo.ssantos127@gmail.com>
2022-04-30 10:57:59 -0300

    Initial

---
+++ app/mailers/application_mailer.rb
@@ -0,0 +1,4 @@
+class ApplicationMailer < ActionMailer::Base
+  default from: 'marcelo.jhg@hotmail.com'
+  layout 'mailer'
+end
---
+++ app/models/product.rb
@@ -0,0 +1,14 @@
+class Product < ApplicationRecord
+  belongs_to :productable, polymorphic: true
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
+  validates :description, presence: true
+  validates :price, presence: true, numericality: { greater_than: 0 }
+
+  belongs_to :productable, polymorphic: true
+  has_many :product_categories, dependent: :destroy
+  has_many :categories, through: :product_categories
+
+  has_one_attached :image
+  validates :price, presence: true, numericality: { greater_than: 0 }
+  validates :image, presence: true
+end
